c82ac447d196c5d804cdbda4fac7170eb4537248
Rupesh Tripathi <rupeshstripathi1234@gmail.com>
2025-05-16 15:02:50 +0530

    Connected with MySQL locally

--- src/main/java/com/app/todoapp/repository/TaskRepository.java
+++ src/main/java/com/app/todoapp/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.app.todoapp.repository;
 
-public class TaskRepository {
+import com.app.todoapp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
